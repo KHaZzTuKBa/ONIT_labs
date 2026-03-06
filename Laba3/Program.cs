73a4a90909adf2f500572c91b1240f72855fcea6
Nikolay <zamock15@gmail.com>
2026-03-06 18:46:16 +0300

    222

--- Laba3/Program.cs
+++ Laba3/Program.cs
@@ -26,7 +26,11 @@ namespace Laba3
 			using (var scope = app.Services.CreateScope())
 			{
 				var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-				db.Database.Migrate();
+
+				if (db.Database.IsRelational())
+				{
+					db.Database.Migrate();
+				}
 			}
 
 			// Configure the HTTP request pipeline.
